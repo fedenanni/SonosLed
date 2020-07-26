4772393627bd5cd6ac1b2db3c3252def4ebfefbb
Federico Nanni <fedenanni@users.noreply.github.com>
2020-07-26 09:52:32 +0100

    Updating local paths

--- check_all.py
+++ check_all.py
@@ -2,7 +2,7 @@ import subprocess
 import time
 while True:
 	try:
-		subprocess.call("npm restart & python2 ../luma.led_matrix/examples/fede-giu.py --cascaded 4 --block-orientation -90", shell=True)
+		subprocess.call("npm restart & python2 fede-giu.py --cascaded 4 --block-orientation -90", shell=True)
 	except: #catch everything else
 		print ("ups")
 		conitnue
